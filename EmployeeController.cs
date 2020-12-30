cb8c6c5f1af5e9cd5d57b2363f3217b4d8fd48a5
Sajeed-Syn <67855906+Sajeed-Syn@users.noreply.github.com>
2020-12-30 09:00:47 +0530

    Add files via upload

---
+++ EmployeeController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DemoMvc.Models;
+namespace DemoMvc.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        List<Employee> emplist = null;
+        public void CreateList()
+        {
+             emplist = new List<Employee>
+            {
+                new Employee{Empno=101,Name="Tom",Salary=40000,Dept="HR"},
+                new Employee{Empno=102,Name="Jerry",Salary=70000,Dept="TR"},
+                new Employee{Empno=103,Name="Raj",Salary=80000,Dept="HR"},
+                new Employee{Empno=104,Name="Ali",Salary=60000,Dept="TR"},
+                new Employee{Empno=105,Name="Jay",Salary=30000,Dept="HR"},
+                new Employee{Empno=106,Name="Bob",Salary=20000,Dept="TR"}
+            };
+        }
+        [HttpGet]
+        public ActionResult Search()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Search(int empid)
+        {
+            CreateList();
+            var data = emplist.Where(emp => emp.Empno == empid).FirstOrDefault();
+            ViewBag.Emp = data;
+            return View();
+        }
+        public ActionResult Index()
+        {
+            DateTime dt = DateTime.Now;
+            ViewBag.Date = dt;
+            string name = "Tom";
+            ViewBag.Name = name;
+            TempData["Name"] = ViewBag.Name;
+            return View();
+        }
+        public ActionResult Display()
+        {
+            ViewBag.Name = TempData["Name"];
+
+            Employee emp = new Employee { Empno = 101, Name = "Tom", Salary = 70000, Dept = "HR" };
+            return View(emp);
+        }
+        public ActionResult List()
+        {
+            CreateList();
+            ViewBag.Name = TempData["Name"];
+            
+            // extension methods
+            int count = emplist.Count();
+            int total = emplist.Sum(emp => emp.Salary);
+            ViewData["Count"] = count;
+            ViewBag.TotalSalary = total;
+            ViewBag.Name = TempData["Name"];
+            return View(emplist);
+        }
+
+    }
+}
---
+++ MoviesController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DemoMvc.Models;
+namespace DemoMvc.Controllers
+{
+    public class MoviesController : Controller
+    {
+        List<Movies> movielist = null;
+        public void CreateList()
+        {
+            movielist = new List<Movies>
+            {
+                new Movies{MovieId=1,Name="Gold",Year=2018,Rating=4},
+                  new Movies{MovieId=2,Name="LootCase",Year=2020,Rating=3},
+                    new Movies{MovieId=3,Name="Gunjan",Year=2020,Rating=2},
+                      new Movies{MovieId=4,Name="War",Year=2019,Rating=4},
+                        new Movies{MovieId=5,Name="Ludo",Year=2020,Rating=3}
+
+            };
+        }
+        public ActionResult Show(int movieid)
+        {
+            CreateList();
+            var movie = movielist.Where(mov=>mov.MovieId==movieid).FirstOrDefault();
+            return View(movie);
+        }
+        public ActionResult Search()
+        {
+            return View();
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Display()
+        {
+            Movies movies = new Movies { MovieId = 1, Name = "Gold", Year = 2018, Rating = 4 };
+            return View(movies);
+        }
+        public ActionResult List()
+        {
+            CreateList();
+            return View(movielist);
+        }
+    }
+}
